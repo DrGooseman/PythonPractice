1f6359e203de196e706d309b98dd66b2c36cfec1
DrGooseman <jamesjquinn12@gmail.com>
2020-07-17 21:43:03 +0300

    adds basic routing/pages

---
+++ trydjango/pages/views.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+
+
+def home_view(request, *args, **kwargs):
+    return HttpResponse("<h1>Hello World</h1>")
+
+
+def contact_view(request, *args, **kwargs):
+    return HttpResponse("<h1>Contact Page</h1>")
+
+
+def about_view(request, *args, **kwargs):
+    return HttpResponse("<h1>About Page</h1>")
+
+
+def social_view(request, *args, **kwargs):
+    return HttpResponse("<h1>Social Page</h1>")
