45901a5421c7ece18c3f9c3e7afb347341cca52c
DrGooseman <jamesjquinn12@gmail.com>
2020-07-18 22:41:26 +0300

    adds basic templates with includes

--- trydjango/pages/views.py
+++ trydjango/pages/views.py
@@ -5,15 +5,15 @@ from django.http import HttpResponse
 
 
 def home_view(request, *args, **kwargs):
-    return HttpResponse("<h1>Hello World</h1>")
+    return render(request, "home.html", {})
 
 
 def contact_view(request, *args, **kwargs):
-    return HttpResponse("<h1>Contact Page</h1>")
+    return render(request, "contact.html", {})
 
 
 def about_view(request, *args, **kwargs):
-    return HttpResponse("<h1>About Page</h1>")
+    return render(request, "about.html", {})
 
 
 def social_view(request, *args, **kwargs):
